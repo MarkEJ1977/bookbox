6f074cc4681c7a2b3d0f6517854468d83ebc472a
Mark Ellis-Jones <markellisjones@gmail.com>
2012-11-28 09:35:01 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookbox::Application.config.secret_token = '486094f4695ca1091d3ae63414bb71d76be0941c55ed208370b117c77b353698c5d51b5bdaf057300557551bfc265a2c44c4ec0e241f83d3996c08402ad23673'
